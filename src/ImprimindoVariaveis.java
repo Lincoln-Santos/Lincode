698fbb5e73282da56ffcc71fa3303c6f6bdfc5e3
LinconleCíntia <lincoln_santos@outlook.com.br>
2020-04-19 02:51:40 -0300

    Curso Java

---
+++ src/ImprimindoVariaveis.java
@@ -0,0 +1,16 @@
+public class ImprimindoVariaveis {
+
+    public static void  main(String[] args) {
+        int idade = 10;
+        double salarioDouble = 3000;
+        float salarioFloat = 3000;
+        byte idadeByte = 12;
+        short idadeShort = 32767;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        long numeroGrande = 1000L;
+        char caractere = '\u0041'; // 2 bytes
+        String nome = "Lincoln";
+        System.out.println(salarioFloat);
+    }
+}
---
+++ src/RelatorioPendencia.java
@@ -0,0 +1,12 @@
+public class RelatorioPendencia {
+
+    public static void main(String[] args) {
+
+        String nome = "Lincoln";
+        String endereco = "439 East Distric" ;
+        String telefone = "(11) 99641-0576";
+
+        System.out.println("O " + nome + " domiciliado no endereço " + endereco + " e telefone " + telefone + " Não possui pendencias ");
+    }
+
+}

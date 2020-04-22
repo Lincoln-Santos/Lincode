adfd0446b64bcf119bb1b41118c279e484fb955d
LinconleCíntia <lincoln_santos@outlook.com.br>
2020-04-22 01:58:14 -0300

    Estudos de Array´s MultiDimencionais

---
+++ src/Arrays4.java
@@ -0,0 +1,13 @@
+public class Arrays4 {
+
+    public static void main(String[] args) {
+
+        String [] nomes =  {"Lincoln", "Agostinho", "dos", "Santos"};
+
+        for(String nome : nomes){
+
+            System.out.println(nome);
+        }
+
+    }
+}
